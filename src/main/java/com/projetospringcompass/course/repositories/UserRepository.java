faea26d4cba3a6a5a9897ad1cd7a44e07562a977
Pedro <pedroluc1232@gmail.com>
2024-05-07 12:47:47 -0300

    :sparkles: JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/projetospringcompass/course/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.projetospringcompass.course.repositories;
+
+import com.projetospringcompass.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
+
+}
